494a5891505af56364da17c26d3d694346ae25f7
xl-wolf <1072236984@qq.com>
2022-07-06 16:32:01 +0800

    init

---
+++ auto_login.py
@@ -0,0 +1,83 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from time import sleep
+from selenium.webdriver import ActionChains
+import computeLeft
+import os
+
+
+screen_shot_dir = "./screen_shot_dir/"
+
+if not os.path.exists(screen_shot_dir):
+    os.makedirs(screen_shot_dir)
+
+
+def inupt_characters(characters, input_ele):
+    for char in characters:
+        input_ele.send_keys(char)
+        sleep(0.1)
+
+
+def login():
+    javascript = 'window.location.href="https://upass.10jqka.com.cn/login"'
+    driver.execute_script(javascript)
+    sleep(1)
+    driver.switch_to.window(driver.window_handles[-1])
+    driver.find_element(By.XPATH, '//*[@id="to_account_login"]/a').click()
+    uname_ele = driver.find_element(By.XPATH, '//*[@id="uname"]')
+    pwd_ele = driver.find_element(By.XPATH, '//*[@id="passwd"]')
+    uname = 'xyja71'
+    password = '123456'
+    inupt_characters(uname, uname_ele)
+    inupt_characters(password, pwd_ele)
+    sleep(1)
+    driver.find_element(By.XPATH, '//*[@id="account_pannel"]/div[4]').click()
+    sleep(2)
+    slide()
+
+
+def slide():
+    slide_btn = driver.find_element(By.XPATH, '//*[@id="slicaptcha-block"]')
+    # 截图保存为target.png
+    slide_btn.screenshot(r''+screen_shot_dir+'target.png')
+    # 使用js隐藏方块
+    js = "document.getElementById('slicaptcha-block').style['visibility'] = 'hidden'"
+    driver.execute_script(js)
+    sleep(1)
+    bg_img = driver.find_element(By.XPATH, '//*[@id="slicaptcha-img"]')
+    # 截图保存为background.png
+    bg_img.screenshot(r''+screen_shot_dir+'background.png')
+    # 截图后使用js显示方块
+    js = "document.getElementById('slicaptcha-block').style['visibility'] = 'visible'"
+    driver.execute_script(js)
+    sleep(2)
+
+    distance = computeLeft.detect_displacement(r''+screen_shot_dir+'target.png', r''+screen_shot_dir+'background.png')
+    # 点击滑块
+    slider = driver.find_element(By.XPATH, '//*[@id="slider"]')
+    zero_distance = distance*0.1
+    first_distance = distance*0.1
+    second_distance = distance*0.7
+    third_distance = distance*0.1
+    actionChains = ActionChains(driver)
+    actionChains.click_and_hold(slider).perform()
+
+    actionChains.move_by_offset(zero_distance, 1).perform()
+    sleep(0.2)
+    actionChains.move_by_offset(first_distance, 0).perform()
+    sleep(0.1)
+    actionChains.move_by_offset(second_distance, -1).perform()
+    sleep(0.2)
+    actionChains.move_by_offset(third_distance, 1).perform()
+    sleep(2)
+
+    actionChains.release().perform()
+
+
+driver = webdriver.Chrome()
+driver.implicitly_wait(10)  # 隐式等待时间10s
+driver.maximize_window()
+
+login()
+sleep(300)
+driver.quit()
---
+++ computeLeft.py
@@ -0,0 +1,33 @@
+import cv2
+
+
+def _tran_canny(image):
+    """消除噪声"""
+    image = cv2.GaussianBlur(image, (3, 3), 0)
+    return cv2.Canny(image, 50, 150)
+
+
+def detect_displacement(img_slider_path, image_background_path):
+    """detect displacement"""
+    # # 参数0是灰度模式
+    image = cv2.imread(img_slider_path, 0)
+    template = cv2.imread(image_background_path, 0)
+
+    # 寻找最佳匹配
+    res = cv2.matchTemplate(_tran_canny(
+        image), _tran_canny(template), cv2.TM_CCOEFF_NORMED)
+    # 最小值，最大值，并得到最小值, 最大值的索引
+    min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(res)
+
+    top_left = max_loc[0]  # 横坐标
+    # 展示圈出来的区域
+    x, y = max_loc  # 获取x,y位置坐标
+
+    w, h = image.shape[::-1]  # 宽高
+    cv2.rectangle(template, (x, y), (x + w, y + h), (7, 249, 151), 2)
+    return top_left
+
+
+if __name__ == '__main__':
+    top_left = detect_displacement("target.png", "background.png")
+    print(top_left)
